0581857f6e6465f82f5a95d70b400907969ce444
mariyakhno <marinayakhno31@gmail.com>
2022-07-24 19:17:59 +0200

    Initial commit

---
+++ main1.js
@@ -0,0 +1,33 @@
+let a = 'hello', b = 'owu', c = 'ua', d = 'com', e = 1, f = 10, g = -999, h = 3.14, j = 123, k = 2.7, l = 16, a1 = 'true', a2 = 'false';
+document.write(a,' ',b,' ',c,' ',d,' ',e);
+console.log(a,b,c,d,e,f,g,h,j,k,l,a1,a2);
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+//
+
+
+let firstName = 'Maryna', middleName = 'Ihorevna',
+lastName = 'Yakhno';
+let person = firstName + ' '+ middleName + ' ' + lastName
+console.log(person)
+
+let a3 = 100;
+let b3 = '100';
+let c3 = true;
+console.log(typeof a3, typeof b3, typeof c3);
+
+let inputA = prompt();
+let inputB = prompt();
+let inputC = prompt();
+console.log(inputA, inputB, inputC);
+
+
+
+
+
+
